0461bd1036e5060581586ff7a0871464c944b0e3
LiQiang <jdswee@163.com>
2019-09-17 18:08:28 +0800

    feat webpack

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import '../src/css/index.css'
 import '../src/css/index.less'
 import '../src/css/index.scss'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'open-iconic/font/css/open-iconic-bootstrap.css'
 
-$(function() {
-  $('li:odd').css('backgroundColor', 'lightpink');
-  $('li:even').css('backgroundColor', function() {
-    return '#' + 'D97634'
-  });
-})
+// class 关键字，是 ES6 中用来实现面向对象编程的方式
+class Person {
+  static info = { name: 'zs', age: 20 }
+}
+
+console.log(Person.info)
--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,21 @@ module.exports = {
             loader: 'url-loader'
           }
         ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-transform-runtime', 
+              '@babel/plugin-transform-arrow-functions',
+              '@babel/plugin-proposal-class-properties'
+            ]
+          }
+        }
       }
     ]
   }

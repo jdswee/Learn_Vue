87bc2b579824983c1ec546a0caba4d3e27c20f41
LiQiang <jdswee@163.com>
2019-09-25 22:31:59 +0800

    feat

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -9,10 +9,10 @@
 
     <ul class="mui-table-view mui-grid-view mui-grid-9">
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
+        <router-link to="/Home/newslist">
           <img src="../../img/menu1.png">
           <div class="mui-media-body">新闻资讯</div>
-        </a>
+        </router-link>
       </li>
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
         <img src="../../img/menu2.png">
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Home from './components/tabbar/HomeContainer.vue'
 import Member from './components/tabbar/MemberContainer.vue'
 import ShoppingCar from './components/tabbar/ShoppingCarContainer.vue'
 import Search from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewsList.vue'
 
 var router = new VueRouter({
   routes: [
@@ -10,7 +11,8 @@ var router = new VueRouter({
     { path: '/Home', component: Home },
     { path: '/Member', component: Member },
     { path: '/ShoppingCart', component: ShoppingCar },
-    { path: '/Search', component: Search } 
+    { path: '/Search', component: Search },
+    { path: '/Home/newslist', component: NewsList} 
   ],
   linkActiveClass: 'mui-active'
 })

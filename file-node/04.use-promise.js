94f68149fda5502ff0d7da71034c268f6571d2d9
LiQiang <jdswee@163.com>
2019-09-25 21:42:45 +0800

    feat jquery ajax promise

--- file-node/04.use-promise.js
+++ file-node/04.use-promise.js
@@ -8,13 +8,32 @@ function getFileByPath(fpath) {
     })
   })
 }
+// 第一种方式，前面报错不影响后面的执行
+// getFileByPath('./files/11.txt').then(function (data) {
+//   console.log(data)
+//   return getFileByPath('./files/2.txt')
+// }, function(err) {
+//   console.log(err.message)
+//   return getFileByPath('./files/2.txt')
+// }).then(function (data) {
+//   console.log(data)
+//   return getFileByPath('./files/3.txt')
+// }, function(err) {
+//   console.log(err.message)
+//   return getFileByPath('./files/3.txt')
+// }).then(function (data) {
+//   console.log(data)
+// })
 
-getFileByPath('./files/1.txt').then(function (data) {
+// 第二种方式，前面报错立即停止并打印错误信息
+getFileByPath('./files/1.txt').then(function(data) {
   console.log(data)
-  return getFileByPath('./files/2.txt')
-}).then(function (data) {
+  return getFileByPath('./files/12.txt')
+}).then(function(data) {
   console.log(data)
   return getFileByPath('./files/3.txt')
-}).then(function (data) {
+}).then(function(data) {
   console.log(data)
+}).catch(function(err) {
+  console.log('catch 自己的提示：' + err.message)
 })

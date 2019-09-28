52c807f5ce545c66a546702a004070fecd685eb0
LiQiang <jdswee@163.com>
2019-09-28 21:47:17 +0800

    feat 图片顶部滑动tab高亮显示

--- src/components/photos/Photolist.vue
+++ src/components/photos/Photolist.vue
@@ -3,30 +3,11 @@
     <div id="slider" class="mui-slider">
 				<div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
 					<div class="mui-scroll">
-						<a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-							推荐
-						</a>
-						<a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-							热点
-						</a>
-						<a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-							北京
-						</a>
-						<a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-							社会
-						</a>
-						<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-							娱乐
-						</a>
-            <a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
-							科技
-						</a>
-            <a class="mui-control-item" href="#item7mobile" data-wid="tab-top-subpage-7.html">
-							生活
+						<a :class="['mui-control-item', item.id == 0 ? 'mui-active': '']" v-for="item in categorys" :key="item.id">
+							{{ item.title }}
 						</a>
 					</div>
 				</div>
-
 			</div> 
   </div>
 </template>
@@ -36,15 +17,27 @@ import mui from '../../../lib/mui-master/examples/hello-mui/js/mui.min.js'
 export default {
   data() {
     return {
-
+      categorys: []
     }
+  },
+  created() {
+    this.getCategory()
   },
   mounted() {
     mui('.mui-scroll-wrapper').scroll({
       deceleration: 0.0005
     })
   },
-  methods: {}
+  methods: {
+    getCategory() {
+      this.$http.get('api/getimgcategory').then(response => {
+        if(response.body.status === 0) {
+          response.body.message.unshift({title: '全部', id: 0})
+          this.categorys = response.body.message
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>

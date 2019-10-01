b54a7d206f28d86c7dd4dbd89bb3ac437581a063
LiQiang <jdswee@163.com>
2019-10-01 11:27:50 +0800

    feat 商品列表页

--- src/components/products/ProductList.vue
+++ src/components/products/ProductList.vue
@@ -1,51 +1,55 @@
 <template>
   <div class="good-list">
-    <div class="item">
-      <img src="http://demo.dtcms.net/upload/201504/18/thumb_201504181230434303.jpg" alt="img">
-      <h1 class="title">华为（HUAWEI）荣耀6Plus16G双4G版</h1>
+    <div class="item" v-for="item in goodslist" :key="item.id">
+      <img :src="item.img_url" alt="img">
+      <h1 class="title">{{ item.title }}</h1>
       <div class="info">
         <div class="price">
-          <span class="new">￥2195</span>
-          <span class="old">￥2499</span>
+          <span class="new">￥{{ item.sell_price }}</span>
+          <span class="old">￥{{ item.market_price }}</span>
         </div>
         <div class="detail">
           <span class="hotsale">热卖中</span>
-          <span class="remainder">剩60件</span>
+          <span class="remainder">剩{{ item.stock_quantity }}件</span>
         </div>
       </div>
     </div>
-    <div class="item">
-      <img src="http://demo.dtcms.net/upload/201504/18/thumb_201504181230434303.jpg" alt="img">
-      <h1 class="title">华为（HUAWEI）荣耀6Plus16G双4G版</h1>
-      <div class="info">
-        <div class="price">
-          <span class="new">￥2195</span>
-          <span class="old">￥2499</span>
-        </div>
-        <div class="detail">
-          <span class="hotsale">热卖中</span>
-          <span class="remainder">剩60件</span>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <img src="http://demo.dtcms.net/upload/201504/18/thumb_201504181230434303.jpg" alt="img">
-      <h1 class="title">华为（HUAWEI）荣耀6Plus16G双4G版</h1>
-      <div class="info">
-        <div class="price">
-          <span class="new">￥2195</span>
-          <span class="old">￥2499</span>
-        </div>
-        <div class="detail">
-          <span class="hotsale">热卖中</span>
-          <span class="remainder">剩60件</span>
-        </div>
-      </div>
-    </div>
-  </div>
+    <mt-button type="danger" size="large" @click="getMore">加载更多</mt-button>
+</div>
 </template>
 <script>
+import { Toast } from 'mint-ui'
+
 export default {
+  data() {
+    return {
+      pageIndex: 1,
+      goodslist: []
+    }
+  },
+  created() {
+    this.pageIndex = 1
+    this.getGoodsList()
+  },
+  methods: {
+    getGoodsList() {
+      this.$http.get('api/getgoods?pageindex=' + this.pageIndex).then(response => {
+        if(response.body.status === 0) {
+          // this.goodslist = response.body.message
+          if(response.body.message.length === 0) return Toast('到底了')
+          this.goodslist = this.goodslist.concat(response.body.message)
+        } else {
+          Toast('无数据')
+        }
+      }, response => {
+        Toast(response.statusText)
+      })
+    },
+    getMore() {
+      this.pageIndex++
+      this.getGoodsList()
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>

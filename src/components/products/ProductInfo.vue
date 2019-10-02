1e6bb9f121b6b16f8a3fb937058fc575ef5fbc1c
LiQiang <jdswee@163.com>
2019-10-02 09:41:41 +0800

    feat 商品购买页的数量组件

--- src/components/products/ProductInfo.vue
+++ src/components/products/ProductInfo.vue
@@ -11,10 +11,17 @@
     </div>
     <!-- 商品购买 -->
     <div class="mui-card">
-      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-header">商品名称</div>
       <div class="mui-card-content">
         <div class="mui-card-content-inner">
-          包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）
+          <p class="price">
+            市场价：<del>￥2699</del>  销售价：<span class="now">￥2199</span>
+          </p>
+          购买数量：<number-box></number-box>
+          <div class="btn">
+            <mt-button type="primary" size="small">立即购买</mt-button>
+            <mt-button type="danger" size="small">加入购物车</mt-button>
+          </div>
         </div>
       </div>
     </div>
@@ -32,6 +39,8 @@
 
 <script>
 import swiper from '../subcomponents/swiper.vue'
+import numberBox from '../subcomponents/goodinfo_numberbox.vue'
+
 export default {
   data() {
     return {
@@ -56,7 +65,8 @@ export default {
     }
   },
   components: {
-    'swiper': swiper
+    'swiper': swiper,
+    'number-box': numberBox
   }
 }
 </script>
@@ -65,5 +75,15 @@ export default {
 .productinfo-container {
   overflow: hidden;
   background-color: #eee;
+  .price {
+    .now {
+      color: red;
+      font-weight: bold;
+      font-size: 16px;
+    }
+  }
+  .btn {
+    margin-top: 10px;
+  }
 }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   devServer: {
     open: true,
     // host: '0.0.0.0',
-    port: 8080,
+    port: 8081,
     hot: true,
     // contentBase: path.join(__dirname, 'dist')
   },
